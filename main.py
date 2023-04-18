777aae8e52a4c5546a5d48575818a20ce4740235
Stevan <stevanvlajic5@gmail.com>
2023-04-18 18:59:11 +0200

    main.py: soil-sensor reading, avg calculation

--- main.py
+++ main.py
@@ -1,9 +1,57 @@
+import _thread
 import time
-from machine import ADC, Pin
-import utime
+from machine import Pin, ADC
+
+def calcAvg(vals):
+    sum = 0
+    for i in range(0, len(vals)):
+        sum += vals[i]
+    return sum / len(vals)
+
+def readSoilSensor():
+    
+    # init
+    soil = ADC(Pin(26))
+    delay = 0.5
+    counter = 0
+    vals = []
+
+    # assign the values from the calibrator.py to the vars underneath
+    maxVal = 65535
+    minVal = 0
+
+    while True:
+        if counter <= 10:
+            # get the 16-bit integer value from the sensor
+            currVal = soil.read_u16()
+
+            # calculate the average with this formular: ((max-x)*100) - ()
+            perc_val = ((maxVal-currVal) * 100) / (maxVal-minVal)
+
+            vals.append(perc_val)
+
+            counter = counter + 1
+        else:
+            print("Average humidity for " + "[" + counter + "] iterations: " + calcAvg(vals))
+
+            # reset counter
+            counter = 0
+
+        # set delay to get accurate sensor-values
+        time.sleep(delay)
+        pass
+
+def while2():
+    # Add code for while loop 2 here
+    while True:
+        print("test 2")
+        time.sleep(1)
+        pass
+
+def runLcdDisplay():
+    print("LCD screen is running")
 
 
-soil = ADC(Pin(26))
-
-
-soil.read_u16
+_thread.start_new_thread(readSoilSensor, ())
+_thread.start_new_thread(while2, ())
+_thread.start_new_thread(runLcdDisplay,())
